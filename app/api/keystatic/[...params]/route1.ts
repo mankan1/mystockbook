14ef1f544440af4298942bae97149537d6b9e223
Manoj Kandlikar <manojkandlikar@Manojs-MacBook-Air.local>
2026-03-28 21:51:01 -0700

    ifca

--- app/api/keystatic/[...params]/route1.ts
+++ app/api/keystatic/[...params]/route1.ts
@@ -1,7 +1,4 @@
 import { makeRouteHandler } from '@keystatic/next/route-handler'
 import config from '../../../../keystatic.config'
 
-export const runtime = 'nodejs'
-export const dynamic = 'force-dynamic'
-
 export const { POST, GET } = makeRouteHandler({ config })
